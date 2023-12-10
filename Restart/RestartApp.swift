1fa9a705c610f8aca279cb7184bad5471ff27000
Jonas <88572943+Johnaoe@users.noreply.github.com>
2023-12-10 14:49:48 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Jonas Narbutas on 2023-12-10.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Restart/Screens/HomeView.swift
@@ -0,0 +1,34 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by Jonas Narbutas on 2023-12-10.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+//    MARK: - PROPERTY
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    
+//    MARK: - BODY
+    var body: some View {
+        VStack (spacing: 20) {
+            Text("Home")
+                .font(.largeTitle)
+            
+            Button(action: {
+                isOnboardingViewActive = true
+            }){
+                Text("Restart")
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
